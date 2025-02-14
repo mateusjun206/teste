2dd00b7907dbcb8238ec3e3eafd2b266fb2bd27a
Mateusjun206 <jpcosta237@gmail.com>
2025-02-14 17:32:38 -0300

    primeiro commit

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+print("AREA DO QUADRADRO")
+x = int(input("digite o valor do lado"))
+area = x * x
+print("a area do quadradro é: ", area)
+
+print("AREA DO retangulo")
+x = int(input("digite o valor do lado"))
+y = int(input("digite o valor do lado"))
+area = x * y
+print("a area do retangulo é: ", area)
+
+print("AREA DO triangulo")
+x = int(input("digite o valor do lado"))
+y = int(input("digite o valor do lado"))
+area = x * y / 2
+print("a area do triangulo é: ", area)
---
+++ majbk1.py
@@ -0,0 +1,4 @@
+horas = int(input("digite as horas"))
+quantia = int(input("degite o valo"))
+salario = horas*quantia
+print("salario igual a ", salario)
